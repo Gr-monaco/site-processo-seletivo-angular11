99e2226d612a1f27280e827afc74fe83cbe28663
Gabriel Monaco <gr-monaco@hotmail.com>
2021-10-03 11:58:12 -0300

    Adicionar função de titulo de pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
@@ -38,7 +38,9 @@ import { ArticlesSectionComponent } from './articles-section/articles-section.co
     MatIconModule,
     MatSelectModule
   ],
-  providers: [],
+  providers: [
+    Title
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/main-website-body/main-website-body.component.ts
+++ src/app/main-website-body/main-website-body.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Title } from '@angular/platform-browser';
+
 @Component({
   selector: 'main-website-body',
   templateUrl: './main-website-body.component.html',
@@ -10,9 +12,14 @@ export class MainWebsiteBodyComponent implements OnInit {
   cadastroVisivel : boolean = false;
   tabelaVisivel : boolean = false;
 
-  constructor() { }
+  constructor( private titleService: Title) { }
 
   ngOnInit(): void {
+    this.setTitle("Site de programação - Home")
+  }
+
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
   }
 
   updateValueCadastro(val: boolean) {
