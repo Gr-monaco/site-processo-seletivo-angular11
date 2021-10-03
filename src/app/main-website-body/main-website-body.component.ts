bd598ad273d7721429f353f56eda1a9fae79b890
Gabriel Monaco <gr-monaco@hotmail.com>
2021-10-03 12:16:52 -0300

    Mudar o nome da página

--- src/app/main-website-body/main-website-body.component.ts
+++ src/app/main-website-body/main-website-body.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Title } from '@angular/platform-browser';
+import { Title } from '@angular/platform-browser';  
 
 @Component({
   selector: 'main-website-body',
